2a2f1e49fe3eea5396b2b7946611c49c6844557d
Shahilali12 <89555969+Shahilali12@users.noreply.github.com>
2024-04-16 15:57:18 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Set terminal colors
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+MAGENTA="\033[35m"
+CYAN="\033[36m"
+RESET="\033[0m"
+
+# Print banner
+echo -e "${RED}██████╗░░█████╗░░██████╗░███████╗██████╗░░█████╗░████████╗"
+echo -e "██╔══██╗██╔══██╗██╔════╝░██╔════╝██╔══██╗██╔══██╗╚══██╔══╝"
+echo -e "██║░░██║██║░░██║██║░░██╗░█████╗░░██████╔╝███████║░░░██║░░░"
+echo -e "██║░░██║██║░░██║██║░░╚██╗██╔══╝░░██╔══██╗██╔══██║░░░██║░░░"
+echo -e "██████╔╝╚█████╔╝╚██████╔╝███████╗██║░░██║██║░░██║░░░██║░░░"
+echo -e "╚═════╝░░╚════╝░░╚═════╝░╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░${RESET}"
+echo -e "${YELLOW}               ░D░O░G░E░ ░R░A░T░B░Y░S░ ░H░I░V░ ░A░Y░A░D░A░V░${RESET}"
+
+
+echo -e "\e[0m"
+echo "Author: shivayadv"
+echo "Github: shivaya-dav"
+echo "Telegram Id: @ShahilGod"
+echo -n "Loading "
